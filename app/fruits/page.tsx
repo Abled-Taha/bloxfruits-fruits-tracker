f1a21ef79b017a9be5f3b042389d3f90ea4d5e08
Abled-Taha <abledtaha@gmail.com>
2025-09-10 15:16:27 +0500

    Added boilerplate un empty pages

---
+++ app/fruits/page.tsx
@@ -0,0 +1,7 @@
+export default function FruitsPage() {
+  return (
+    <main>
+      <h1>Fruits</h1>
+    </main>
+  );
+}
---
+++ app/saver/page.tsx
@@ -0,0 +1,7 @@
+export default function SaverPage() {
+  return (
+    <main>
+      <h1>Saver</h1>
+    </main>
+  );
+}
